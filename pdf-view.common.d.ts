879fc3a788606d074825ea4319ed5754b625a191
Luca Cerretani <luca.cerretani@gmail.com>
2020-07-28 11:22:16 +0200

    accept base64 data in src

--- pdf-view.common.d.ts
+++ pdf-view.common.d.ts
@@ -2,6 +2,9 @@ import { Property, View } from 'tns-core-modules/ui/core/view';
 export declare abstract class PDFViewCommon extends View {
     enableAnnotationRendering: boolean;
     static loadEvent: string;
+    protected tempFolder;
+    public loadPDF(src: string): void;
+    protected createTempFile(base64data: any): void;
     src: string;
     static notifyOfEvent(eventName: string, pdfViewRef: WeakRef<PDFViewCommon>): void;
 }
