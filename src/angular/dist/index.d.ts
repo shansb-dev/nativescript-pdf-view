b51ba8a198e2c1f63f86f3ae63ed6dc4974c334f
shansb-dev <Sebastian.Hansbauer@secadm.de>
2020-10-19 19:14:18 +0200

    fix: added missing angular compilation files, setup

--- src/angular/dist/index.d.ts
+++ src/angular/dist/index.d.ts
@@ -1,9 +1,4 @@
 import { NativeScriptCommonModule } from "@nativescript/angular";
-import * as ɵngcc0 from '@angular/core';
 export declare class PDFViewModule {
     imports: [NativeScriptCommonModule];
-    static ɵmod: ɵngcc0.ɵɵNgModuleDefWithMeta<PDFViewModule, never, never, never>;
-    static ɵinj: ɵngcc0.ɵɵInjectorDef<PDFViewModule>;
 }
-
-//# sourceMappingURL=index.d.ts.map
